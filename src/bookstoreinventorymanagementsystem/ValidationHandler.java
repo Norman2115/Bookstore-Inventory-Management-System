34ff170dae977c0a771e3a5d631a1cbb5de81592
normanteo <normantch04@gmail.com>
2024-03-30 07:26:01 +0100

    Added method checking username uniqueness in db

--- src/bookstoreinventorymanagementsystem/ValidationHandler.java
+++ src/bookstoreinventorymanagementsystem/ValidationHandler.java
@@ -1,5 +1,10 @@
 package bookstoreinventorymanagementsystem;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  *
  * @author Norman
@@ -8,17 +13,38 @@ public class ValidationHandler {
 
     public static ValidationResult isValidUsername(String username) {
         if (username.length() < 6 || username.length() > 16) {
-            return new ValidationResult(false, "Username must be between "
-                    + "6 and 16 characters long");
+            return new ValidationResult(false, "Username must be between 6 and 16 characters long");
         }
-        
+
         return new ValidationResult(true, null);
+    }
+
+    public static ValidationResult isUsernameUnique(String username) throws SQLException {
+        try {
+            DatabaseManager.connect();
+            Connection connection = DatabaseManager.getConnection();
+
+            PreparedStatement statement = connection.prepareStatement(
+                    "SELECT * FROM users WHERE username = ?"
+            );
+
+            statement.setString(1, username);
+
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                return new ValidationResult(false, "The username is already exists in the database.");
+            }
+            
+            return new ValidationResult(true, null);
+        } finally {
+            DatabaseManager.closeConnection();
+        }
     }
 
     public static ValidationResult isValidPassword(String password) {
         if (password.length() < 8 || password.length() > 16) {
-            return new ValidationResult(false, "Password must be between "
-                    + "8 and 16 characters long");
+            return new ValidationResult(false, "Password must be between 8 and 16 characters long");
         }
 
         boolean hasDigit = false;
@@ -36,10 +62,9 @@ public class ValidationHandler {
         }
 
         if (!hasDigit || !hasLetter || !hasSymbol) {
-            return new ValidationResult(false, "Password must be "
-                    + "a combination of numbers, letters and symbols");
+            return new ValidationResult(false, "Password must be a combination of numbers, letters and symbols");
         }
-        
+
         return new ValidationResult(true, null);
     }
 
--- src/bookstoreinventorymanagementsystem/ValidationResult.java
+++ src/bookstoreinventorymanagementsystem/ValidationResult.java
@@ -5,18 +5,19 @@ package bookstoreinventorymanagementsystem;
  * @author Norman
  */
 public class ValidationResult {
+
     private final boolean isValid;
     private final String errorMessage;
-        
+
     public ValidationResult(boolean isValid, String errorMessage) {
         this.isValid = isValid;
         this.errorMessage = errorMessage;
     }
-    
+
     public boolean isValid() {
         return isValid;
     }
-    
+
     public String getErrorMessage() {
         return errorMessage;
     }
