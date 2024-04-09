48c10b882dd7453eb9dc4efa3c0bc26ca0de5d83
wy <liewwenyen@gmail.com>
2024-04-09 15:31:50 +0100

    SalespersonHomepage

--- src/bookstoreinventorymanagementsystem/ColorManager.java
+++ src/bookstoreinventorymanagementsystem/ColorManager.java
@@ -10,7 +10,6 @@ public class ColorManager {
 
     // Note: The comments on top of each color are just suggestions.
     // You can use these colors wherever apppropriate
-    
     // Family Mart Primary Colors
     // Used for button and background color
     public static final Color PRIMARY_BLUE = new Color(0, 140, 214);
@@ -18,23 +17,25 @@ public class ColorManager {
     public static final Color PRIMARY_GREEN = new Color(62, 164, 52);
     // Used for background color
     public static final Color PRIMARY_WHITE = new Color(253, 252, 248);
-    
+
     // WHITE
     // Used for text color
     public static final Color WHITE = new Color(255, 255, 255);
-    
+
     // BLACK
     // Used for text color
     public static final Color BLACK = new Color(30, 30, 30);
-    
+
     // BLUE
+    // Used for hover effect
+    public static Color LIGHT_BLUE = new Color(204, 204, 255);
     // Used for hover effect
     public static Color MEDIUM_BLUE = new Color(0, 113, 176);
     // Used for click effect
     public static Color DEEP_BLUE = new Color(0, 92, 143);
     // Used for text color
     public static Color DARK_BLUE = new Color(0, 0, 139);
-    
+
     // GREEN
     public static final Color LIGHT_GREEN = new Color(0, 200, 0);
     // Used for hover effect
@@ -43,10 +44,10 @@ public class ColorManager {
     public static final Color DEEP_GREEN = new Color(32, 104, 22);
     // Used for text color
     public static final Color DARK_GREEN = new Color(0, 100, 0);
-
+    
     // RED
     public static final Color RED = new Color(255, 0, 0);
-    
+
     // GREY
     // Greys are used for certain elements
     public static final Color LIGHT_GREY = new Color(230, 230, 230);
